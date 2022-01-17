25ef67381c39cec2d53392ae8002d7e0e42e6945
trGiang99 <dtr.giang.1299@gmail.com>
2022-01-17 23:04:10 +0700

    add translation for comments in activation functions code

--- code/activation_functions.py
+++ code/activation_functions.py
@@ -1,51 +1,47 @@
-import math
 import numpy as np
 
-### Note ###
+### Chú thích ###
 
-# z is weighted input
+# z là đầu vào hàm kích hoạt
 
 
-### Functions ###
+### Các hàm kích hoạt ###
 
-def linear(z,m):
+def linear(z, m):
 	return m*z
 
-def elu(z,alpha):
-	return z if z >= 0 else alpha*(e^z -1)
+def elu(z, alpha):
+	return z if z >= 0 else alpha*(np.exp(z) - 1)
 
 def leakyrelu(z, alpha):
 	return max(alpha * z, z)
 
 def relu(z):
-  return max(0, z)
+  	return max(0, z)
 
 def sigmoid(z):
-  return 1.0 / (1 + np.exp(-z))
+  	return 1.0 / (1 + np.exp(-z))
 
 def tanh(z):
 	return (np.exp(z) - np.exp(-z)) / (np.exp(z) + np.exp(-z))
 
 
+### Các đạo hàm ###
 
-
-### Derivatives ###
-
-def linear_prime(z,m):
+def linear_prime(z, m):
 	return m
 
-def elu_prime(z,alpha):
+def elu_prime(z, alpha):
 	return 1 if z > 0 else alpha*np.exp(z)
 
 def leakyrelu_prime(z, alpha):
 	return 1 if z > 0 else alpha
 
 def sigmoid_prime(z):
-  return sigmoid(z) * (1-sigmoid(z))
+  	return sigmoid(z) * (1-sigmoid(z))
 
 def relu_prime(z):
-  return 1 if z > 0 else 0
+  	return 1 if z > 0 else 0
 
 def tanh_prime(z):
 	return 1 - np.power(tanh(z), 2)
-
